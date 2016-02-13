70378ea566e3fefef874dc153c24d022b9b92660
Garysoccer <garytsai@garys-MacBook-Air.local>
2016-02-13 12:24:51 -0500

    Added comments to insertionOfficieal.cpp

--- insertionOfficial.cpp
+++ insertionOfficial.cpp
@@ -4,29 +4,29 @@
 
 using namespace std;
 
-void insertionSort(int arr[],int size) {
+void insertionSort(int arr[],int size) {// take array and its size
 	int i, j, key;
-	for (j = 1; j < size; j++) 
+	for (j = 1; j < size; j++) // starting at second index so we can compare with first index
 	{
-		key = arr[j];
-		i=j-1;
+		key = arr[j];// storing the index to a temperary location
+		i=j-1;//  [i] will use as array subscript 
 		
-		while (i >= 0 && arr[i] > key) {
-				arr[i+1]= arr[i];
+		while (i >= 0 && arr[i] > key) {// comparing the index[i] is graater than [0] and greater than Temperary
+				arr[i+1]= arr[i];//if the condition is true, store index[i] to array list
 				i--;
 					}	
-		arr[i+1]=key;
+		arr[i+1]=key;// if the condition is false, store Temperary to array list
 		
 	}
 	//cout<< arr[i]<<endl;
 }
 int main (){
-clock_t startTime =clock();
+clock_t startTime =clock();// strt counting time
 
 int x;
 cout<<"Enter Amount of random number to be generated >> ";
 cin >> x;
-int ar[x];
+int ar[x];// using static to perform a large sorting
 
 for (int i =0; i<x;i++){
 	ar[i]= arc4random()%x+1;// not that this is matter, but i use arc4random to generate pure randomness of number. 
--- merge.cpp
+++ merge.cpp
@@ -7,54 +7,55 @@
 
 using namespace std;
 void merge(int *,int, int , int );
+
 void mergesort(int *A, int p, int r)
 {
     int q;
-    if (p < r)
+    if (p < r)// checking to see if the last array value is bigger than the first array array value
     {
-        q=(p+r)/2;
-        mergesort(A,p,q);
-        mergesort(A,q+1,r);
-        merge(A,p,q,r);
+        q=(p+r)/2;// if so, break the array to half and store it to mid point
+        mergesort(A,p,q);// check from the first array value to mid point. Break them to smaller if necessary
+        mergesort(A,q+1,r);// ccheck from the array next to the mid point to the end. Break them to smaller if necessary
+        merge(A,p,q,r);// when the array is break into single array size, sort them back. (Divide and conqure)
     }
     return;
 }
 void merge(int *A, int p, int q, int r)// p=begining, q=mid-point, p+1 = one array after mid point, r= end point
 {
     int i, j, k;// test up to 10000 random numbers
-   int* c= new int [100000000];
-    i = p;
-    k = p;
-    j = q + 1;
+   static int c[1000000000];// able to sort  billion number (as long as your computer can handle the amount of memory)
+    i = p;// index [i] from first half of the array
+    k = p;// index [k] for the count of the beginning of the whole array after combining 
+    j = q + 1;// index[j] for second half of the array
    // cout<<"**"<<&a<<endl;
-    while (i <= q && j <= r)
+    while (i <= q && j <= r)// index [i] comparing to midpoint q, index [j] comparing to end point r
     {
-        if (A[i] < A[j])
+        if (A[i] < A[j])// comparing 2 seperate index to see which one is the smaller
         {
-            c[k] = A[i];
+            c[k] = A[i];// if so, store inex [i] it to index [k]
             k++;
             i++;
         }
         else
         {
-            c[k] = A[j];
+            c[k] = A[j];// else, store index [j] to index [k]
             k++;
             j++;
         }
     }
-    while (i <= q)
+    while (i <= q)// moving index [i] to the next array value
     {
         c[k] = A[i];
         k++;
         i++;
     }
-    while (j <= r)
+    while (j <= r) // moving index [j] to the next array value
     {
         c[k] = A[j];
         k++;
         j++;
     }
-    for (i = p; i < k; i++)
+    for (i = p; i < k; i++)//sorting the valuse from back to A 
     {
         A[i] = c[i];
     }
@@ -70,12 +71,12 @@ clock_t startTime =clock();
     int a[arr];
 
 for (i =0; i<arr;i++){
-    a[i]= arc4random()%arr+1;// not that this is matter, but i use arc4random to generate pure randomness of number. 
+    a[i]= arc4random()%arr;// not that this is matter, but i use arc4random to generate pure randomness of number. 
     cout<<a[i]<<" ";
 }
 
 mergesort(a, 0, arr);
-cout<<endl<<"============== sorted array ============="<<endl;
+cout<<endl<<'\t'<<"============================================="<<endl;
     for (i = 0; i < arr; i++)
     {
         cout<<a[i]<<" ";
